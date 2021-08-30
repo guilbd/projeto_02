55e8105cc97cbf73e2f43bb8517172474bee62eb
guilbd@hotmail.com <guilbd@hotmail.com>
2021-08-30 19:53:23 -0300

    new index.js

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const expression = require('express');
+const express = require('express');
 const mongodb = require('mongodb');
+
+const ObjectId = mongodb.ObjectId;
+
+(async => {
+
+    const app = express();
+
+    app.use(express.json());
+
+    const port = 3000;
+
+    app.get('/', (req, res) => {
+        res.send({ info: 'olá mundo'});
+    });
+
+    // app.get('/personagens', async (req, res) => {
+    //     res.send()
+    // })
+
+    app.listen(port, () => {
+        console.info(`App rodando em http://localhost:${port}`);
+
+    })
+
+})();

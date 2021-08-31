9630768db47250bea0bd57dd6e5cd5790bf6b41a
guilbd@hotmail.com <guilbd@hotmail.com>
2021-08-30 21:29:12 -0300

    Update index.js
    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongodb = require('mongodb');
 
 const ObjectId = mongodb.ObjectId;
 
-(async => {
+(async () => {
 
     const app = express();
 
@@ -12,18 +12,34 @@ const ObjectId = mongodb.ObjectId;
     const port = 3000;
 
     const connectionString = `mongodb://localhost:27017/blue_db`;
+    
+    const options = {
+        useUnifiedTopology: true,
+    };
 
+    const client = await mongodb.MongoClient.connect(connectionString, options);
+
+    const db = client.db('blue_db');
+
+    const personagens = db.collection('personagens');
+
+    const getPersonagensValidas = () => personagens.find({}).toArray();
+
+    const getPersonagemById = async(id) => personagens.findOne({_id: ObjectId(id)});
+    
     app.get('/', (req, res) => {
         res.send({ info: 'olá mundo'});
     });
 
-    // app.get('/personagens', async (req, res) => {
-    //     res.send()
-    // })
+
+
+    app.get('/personagens', async (req, res) => {
+        res.send(await getPersonagensValidas());
+    });
 
     app.listen(port, () => {
         console.info(`App rodando em http://localhost:${port}`);
 
-    })
+    });
 
 })();

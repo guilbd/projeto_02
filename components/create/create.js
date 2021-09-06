3c9723849fa339efbcf194b0f048ae4cc7617c91
violigon <vinicius@oligon.com.br>
2021-09-06 16:32:48 -0300

    Update create.js

--- components/create/create.js
+++ components/create/create.js
@@ -3,51 +3,51 @@ const router = express.Router();
 const mongodb = require("mongodb");
 const ObjectId = mongodb.ObjectId;
 (async () => {
-  const dbUser = process.env.DB_USER;
-  const dbPassword = process.env.DB_PASSWORD;
-  const dbName = process.env.DB_NAME;
-  const dbChar = process.env.DB_CHAR;
-  const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-  const options = {
-    useUnifiedTopology: true,
-  };
-  const client = await mongodb.MongoClient.connect(connectionString, options);
+	const dbUser = process.env.DB_USER;
+	const dbPassword = process.env.DB_PASSWORD;
+	const dbName = process.env.DB_NAME;
+	const dbChar = process.env.DB_CHAR;
+	const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+	const options = {
+		useUnifiedTopology: true,
+	};
+	const client = await mongodb.MongoClient.connect(connectionString, options);
 
-  const db = client.db("blue_db");
-  const personagens = db.collection("personagens");
+	const db = client.db("blue_db");
+	const personagens = db.collection("personagens");
 
-  const getPersonagemById = async (id) =>
-    personagens.findOne({ _id: ObjectId(id) });
+	const getPersonagemById = async (id) =>
+		personagens.findOne({ _id: ObjectId(id) });
 
-  //Middleware - especifica que é esse o import do router no index que queremos utilizar
-  router.use(function timelog(req, res, next) {
-    next();
-    console.log("Time: ", Date.now());
-  });
+	//Middleware - especifica que é esse o import do router no index que queremos utilizar
+	router.use(function timelog(req, res, next) {
+		next();
+		console.log("Time: ", Date.now());
+	});
 
-  //[POST] Adicona personagem
-  router.use("/", async (req, res) => {
-    const objeto = req.body;
+	//[POST] Adicona personagem
+	router.use("/", async (req, res) => {
+		const objeto = req.body;
 
-    if (!objeto || !objeto.nome || !objeto.imagemUrl) {
-      res.status(400).send({
-        error:
-          "Personagem inválido, certifique-se que tenha os campos nome e imagemUrl",
-      });
-      return;
-    }
+		if (!objeto || !objeto.nome || !objeto.imagemUrl) {
+			res.status(400).send({
+				error:
+					"Personagem inválido, certifique-se que tenha os campos nome e imagemUrl",
+			});
+			return;
+		}
 
-    const result = await personagens.insertOne(objeto);
+		const result = await personagens.insertOne(objeto);
 
-    console.log(result);
-    //Se ocorrer algum erro com o mongoDb esse if vai detectar
-    if (result.acknowledged == false) {
-      res.status(500).send({ error: "Ocorreu um erro" });
-      return;
-    }
+		console.log(result);
+		//Se ocorrer algum erro com o mongoDb esse if vai detectar
+		if (result.acknowledged == false) {
+			res.status(500).send({ error: "Ocorreu um erro" });
+			return;
+		}
 
-    res.status(201).send(objeto);
-  });
+		res.status(200).send(objeto);
+	});
 })();
 
 module.exports = router;

8b83e7bcea2ce82e2a76d7a659eb464e5bb2fd41
violigon <vinicius@oligon.com.br>
2021-09-06 15:45:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,86 +12,89 @@ const atualizar = require("./components/update/update");
 const criar = require("./components/create/create");
 
 (async () => {
-  const dbUser = process.env.DB_USER;
-  const dbPassword = process.env.DB_PASSWORD;
-  const dbName = process.env.DB_NAME;
-  const dbChar = process.env.DB_CHAR;
+	const dbUser = process.env.DB_USER;
+	const dbPassword = process.env.DB_PASSWORD;
+	const dbName = process.env.DB_NAME;
+	const dbChar = process.env.DB_CHAR;
 
-  const app = express();
-  app.use(express.json());
+	const app = express();
+	app.use(express.json());
 
-  const port = process.env.PORT || 3000;
-  const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+	const port = process.env.PORT || 3000;
+	const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-  const options = {
-    useUnifiedTopology: true,
-  };
+	const options = {
+		useUnifiedTopology: true,
+	};
 
-  console.info("Conectando ao MongoDB Atlas...");
+	console.info("Conectando ao MongoDB Atlas...");
 
-  const client = await mongodb.MongoClient.connect(connectionString, options);
+	const client = await mongodb.MongoClient.connect(connectionString, options);
 
-  console.info("Conexão estabelecida com o MongoDB Atlas!");
+	console.info("Conexão estabelecida com o MongoDB Atlas!");
 
-  const db = client.db("blue_db");
-  const personagens = db.collection("personagens");
+	const db = client.db("blue_db");
+	const personagens = db.collection("personagens");
 
-  const getPersonagensValidas = () => personagens.find({}).toArray();
+	const getPersonagensValidas = () => personagens.find({}).toArray();
 
-  const getPersonagemById = async (id) =>
-    personagens.findOne({ _id: ObjectId(id) });
+	const getPersonagemById = async (id) =>
+		personagens.findOne({ _id: ObjectId(id) });
 
-  //CORS
+	//CORS
 
-  app.all("/*", (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+	app.all("/*", (req, res, next) => {
+		res.header(
+			"Access-Control-Allow-Origin",
+			"https://front-rick-morty-blue.herokuapp.com/"
+		);
 
-    res.header("Access-Control-Allow-Methods", "*");
+		res.header("Access-Control-Allow-Methods", "*");
 
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
-    );
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+		);
 
-    next();
-  });
+		next();
+	});
 
-  //Criando a rota home
-  app.use("/home", home);
+	//Criando a rota home
+	app.use("/home", home);
 
-  // criando a rota read-all
-  app.use("/personagens/read-all", readAll);
+	// criando a rota read-all
+	app.use("/personagens/read-all", readAll);
 
-  //[GET] getPersonagemById
+	//[GET] getPersonagemById
 
-  app.use("/personagens/read-by-id/", readById);
+	app.use("/personagens/read-by-id/", readById);
 
-  //[POST] Adicona personagem
-  app.use("/personagens/create/", criar);
+	//[POST] Adicona personagem
+	app.use("/personagens/create/", criar);
 
-  //[PUT] Atualizar personagem
-  app.use("/personagens/update/", atualizar);
+	//[PUT] Atualizar personagem
+	app.use("/personagens/update/", atualizar);
 
-  //[DELETE] Deleta um personagem
-  app.use("/personagens/delete/", del);
+	//[DELETE] Deleta um personagem
+	app.use("/personagens/delete/", del);
 
-  //Tratamento de erros
-  //Middleware verificar endpoints
-  app.all("*", function (req, res) {
-    res.status(404).send({ message: "Endpoint was not found" });
-  });
+	//Tratamento de erros
+	//Middleware verificar endpoints
+	app.all("*", function (req, res) {
+		res.status(404).send({ message: "Endpoint was not found" });
+	});
 
-  //Middleware -> Tratamento de erro
-  app.use((error, req, res, next) => {
-    res.status(error.status || 500).send({
-      error: {
-        status: error.status || 500,
-        message: error.message || "Internal Server Error",
-      },
-    });
-  });
+	//Middleware -> Tratamento de erro
+	app.use((error, req, res, next) => {
+		res.status(error.status || 500).send({
+			error: {
+				status: error.status || 500,
+				message: error.message || "Internal Server Error",
+			},
+		});
+	});
 
-  app.listen(port, () => {
-    console.info(`App rodando em http://localhost:${port}/home`);
-  });
+	app.listen(port, () => {
+		console.info(`App rodando em http://localhost:${port}/home`);
+	});
 })();
